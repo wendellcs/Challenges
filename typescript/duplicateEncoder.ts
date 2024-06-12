953c0bd7a652ac5af9f1d6aecbeb2b42b399ccd2
Wendell <wendellcs131@gmail.com>
2024-06-12 01:49:25 -0300

    Finished TS challenge

---
+++ typescript/duplicateEncoder.ts
@@ -0,0 +1,23 @@
+export function duplicateEncode(word: string) {
+    let result: string = ''
+    word.toLowerCase().split('').forEach((l: string) => {
+        let count: number = 0
+        for (let i = 0; i < word.length; i++) {
+            word[i].toLowerCase() === l ? count++ : ''
+        }
+        count <= 1 ? result += '(' : result += ')'
+    })
+    return result
+}
+
+// Expected output
+
+// "din"      =>  "((("
+// "recede"   =>  "()()()"
+// "Success"  =>  ")())())"
+// "(( @"     =>  "))((" 
+
+console.log(duplicateEncode("din"))
+console.log(duplicateEncode("recede"))
+console.log(duplicateEncode("Success"))
+console.log(duplicateEncode("(( @"))
